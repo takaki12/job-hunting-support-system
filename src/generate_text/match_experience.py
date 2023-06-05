3a11579110a149aeedeed6e71ea434882e5ceb49
mamemiya-ja <amemiya.masaya.19@shizuoka.ac.jp>
2023-06-05 15:27:10 +0900

    add match_experience

---
+++ src/generate_text/match_experience.py
@@ -0,0 +1,32 @@
+#採用条件と自身の経験のマッチ度を測り、志望動機に組み込むか組み込まないかを決める。
+#返り値　0:マッチしていない　1:一部マッチしている　2:マッチしている　3:エラー
+
+import openai
+
+openai.api_key = ""
+
+#condition:採用条件　experience:自身の経験
+def match_experience(condition, experience):
+    prompt = "以下の採用条件と自身の経験はマッチしていますか。「マッチしている」「一部マッチしている」「マッチしていない」の3択で答えて下さい。\n採用条件：{condition}\n経験：{experience}".format(condition=condition, experience=experience)
+    
+    response = openai.ChatCompletion.create(
+        #gpt4にすることも検討
+        model="gpt-3.5-turbo",
+        #temperatureは一旦0
+        temperature=0,
+        messages=[
+            {"role": "user", "content": prompt}, 
+        ]
+    )
+    
+    response = response["choices"][0]["message"]["content"]
+    
+    if "1部マッチしている" in response:
+        return 1
+    elif "マッチしている" in response:
+        return 2
+    elif "マッチしていない" in response:
+        return 0
+    #エラー処理はまた後で考える（もう一度判定させるのかどうか）
+    else:
+        return 3
