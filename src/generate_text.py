730449920ad0dbca9126cabfcbb44d63de928df6
TettaMasukawa <ea.masukawa17@gmail.com>
2023-06-30 13:45:21 +0900

    add debug

--- src/generate_text.py
+++ src/generate_text.py
@@ -1,5 +1,9 @@
+# import time
+
 def generate(input_text, input_text2, input_text3):
     print(input_text, input_text2, input_text3)
+    # time.sleep(30) # debug: delay 30 seconds
+    
     text = input_text + input_text2 + input_text3 + "を生成しました"
     
     return text
