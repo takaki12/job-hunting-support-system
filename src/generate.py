80e0463a97fc295b20ef45baec5b01aef62e0c11
TettaMasukawa <ea.masukawa17@gmail.com>
2023-07-12 11:31:36 +0900

    delete generate test

--- src/generate.py
+++
@@ -1,9 +0,0 @@
-# import time
-
-def generate(lower_limit, upper_limit):
-    print(lower_limit, upper_limit)
-    # time.sleep(30) # debug: delay 30 seconds
-    
-    text = lower_limit + "\n" + upper_limit + "\n"
-    
-    return text
